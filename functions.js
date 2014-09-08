8367325068d03268744587f17b047c7a74226eb6
Adriano Marinho <adrianomarinhomelo9@gmail.com>
2014-09-08 11:19:22 -0300

    Update functions.js
    Função de acesso nos avas e de unchek nos opções de Revisão das atividades de cada AVA.

--- functions.js
+++ functions.js
@@ -115,3 +115,76 @@ function removeAulaUBC_SyncApp($arrayListVideoAulaName)
 }
 
 //====================================================
+//Função para Acesso nos AVAs e uncheck nos permissões de Revisão das atividades
+//
+$disciplinas = new Array({ava:1457}, {ava:1483}, {ava:1309}, {ava:1499}, {ava:1282}, {ava:1357}, {ava:1348}, {ava:1312}, {ava:1313}, {ava:1461}, {ava:1346}, {ava:1454}, {ava:1485}, {ava:1418}, {ava:1302}, {ava:1305}, {ava:1361}, {ava:1318}, {ava:1481}, {ava:1422}, {ava:1333}, {ava:1386}, {ava:1388}, {ava:1458}, {ava:1489}, {ava:1280}, {ava:1278}, {ava:1491}, {ava:1492}, {ava:1306}, {ava:1469}, {ava:1438}, {ava:1334}, {ava:1463}, {ava:1470}, {ava:1364}, {ava:1495}, {ava:1500}, {ava:1479}, {ava:1439}, {ava:1507}, {ava:1442}, {ava:1387}, {ava:1437}, {ava:1327}, {ava:1515}, {ava:1353}, {ava:1509}, {ava:1360}, {ava:1365}, {ava:1496}, {ava:1407}, {ava:1329}, {ava:1474}, {ava:1343}, {ava:1384}, {ava:1440}, {ava:1320}, {ava:1443}, {ava:1424}, {ava:1326}, {ava:1425});
+
+//chamando a Função de acesso aos AVAs da array
+acessaAVA();
+
+function acessaAVA()
+{
+  $url = "http://ava.brazcubas.br/course/view.php?id=";
+  //$url = "http://54.207.51.47/BETA/course/view.php?id=";
+  for(var $posicao in $disciplinas)
+  {
+    //verificação se dentre o indice da propriedade ava não é string, se não for
+    //ele pega e concatena na url do ava que queremos acessar
+    if(typeof $disciplinas[$posicao].ava != 'string')
+    {
+      _navigateTo($url+$disciplinas[$posicao].ava);
+      //
+      //chama função que desabilita Revisão <<<<<<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      _wait(5000);
+      retiraRevisao();
+      //
+      //
+    }else{
+      _alert($disciplinas[$posicao].ava);
+    }
+
+  };
+};
+
+function retiraRevisao()
+{
+  //busca todos os questionário cadastrados, no AVA atual
+  $collect = _collect('_span','/.* Questionário/', _in(_div('region-content')));
+
+  for ($i = 0; $i < $collect.length; $i++)
+  {
+    //verificando se o modo edição está ativado na pagina do ava atual
+    if(_exists(_submit("Ativar edição")))
+    {
+      _click(_submit("Ativar edição"));
+      _wait(1000);
+    }
+
+    //acessa a url do span atual
+    _click(_parentNode($collect[$i]));
+    _wait(3000);
+    _click(_link("Editar configurações"));
+    _wait(3000);
+    //expande tudo
+    _click(_link("Expandir tudo"));
+    _wait(500);
+
+    //em opções de revisão
+    //uncheck na revisão, exibindo somente nota
+    _uncheck(_checkbox("id_attemptclosed"));
+    _uncheck(_checkbox("id_correctnessclosed"));
+    _uncheck(_checkbox("id_specificfeedbackclosed"));
+    _uncheck(_checkbox("id_generalfeedbackclosed"));
+    _uncheck(_checkbox("id_rightanswerclosed"));
+    _uncheck(_checkbox("id_overallfeedbackclosed"));
+    //_uncheck();
+
+
+
+    //salvando mudanças
+    _click(_submit("Salvar e voltar ao curso"));
+    _wait(1000);
+  };
+
+};
+
